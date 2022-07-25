96f02113af7fa49713117a7ff6727ccfd790923d
domont <d.a.sachkov@gmail.com>
2022-07-25 13:07:52 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CountryPickerView
+
+A description of this package.
---
+++ Tests/CountryPickerViewTests/CountryPickerViewTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CountryPickerView
+
+final class CountryPickerViewTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CountryPickerView().text, "Hello, World!")
+    }
+}
